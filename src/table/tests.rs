b1204d4348b984cf73aa291730e8f9fcd0070d27
Tan Dat <tandat88963820@gmail.com>
2026-07-22 15:08:10 +0700

    [SCRUM-24] add table durability configuration
    fix heap insert 6 bytes header , and freeend by pass 6 header

--- src/table/tests.rs
+++ src/table/tests.rs
@@ -11,3 +11,20 @@ fn create_insert_get() {
     let result = table.get(1).unwrap();
     assert_eq!(result.unwrap().id, 1);
 }
+
+#[test]
+fn open_reindex() {
+    let path = "/tmp/test_open_reindex";
+    let mut table = Table::create(path).unwrap();
+    table
+        .insert(Row {
+            id: 1,
+            fields: vec!["hello".to_string()],
+        })
+        .unwrap();
+    drop(table);
+    let table = Table::open(path).unwrap();
+    let row = table.get(1).unwrap().unwrap();
+    assert_eq!(row.id, 1);
+    assert_eq!(row.fields[0], "hello")
+}
